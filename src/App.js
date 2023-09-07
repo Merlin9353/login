cc500fae3cdba5075595b03fb13fd70228471671
Merlin9353 <eliyat2001@gmail.com>
2023-09-07 16:50:55 +0530

    my first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <button type="button" class="btn btn-success">Primary</button>
+      </header>
+    </div>
+  );
+}
+
+export default App;
